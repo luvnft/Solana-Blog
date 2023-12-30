34ca558ea763da7de1f5012264baf6459f9d4b99
Hahz <hahz5d@pm.me>
2023-12-30 16:43:01 -0500

    code

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom"
-import { App } from "src/App"
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // Check if the path is correct
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
